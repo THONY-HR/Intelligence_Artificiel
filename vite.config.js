cbd8a42bf34239296f7a1d5dcb36c71aa2e685eb
SCARFACEKARA <rantonirinaanthony@gmail.com>
2025-02-19 23:06:33 +0300

    correction port

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: process.env.PORT || 4000,  // Définit le port sur 4000 pour Railway
+    port: process.env.PORT || 9000,  // Définit le port sur 9000 pour Railway
     host: "0.0.0.0",
     strictPort: true   // Empêche Vite de changer le port
   },
   preview: {
-    port: process.env.PORT || 4000,  // Port pour `vite preview`
+    port: process.env.PORT || 9000,  // Port pour `vite preview`
     host: "0.0.0.0",
-    allowedHosts: ["restaurantvue-production.up.railway.app"]
+    allowedHosts: ["ia-anthony.up.railway.app"]
   }
 });
