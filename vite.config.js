c68d5746f2900d595316f27b86f101e306d99544
SCARFACEKARA <rantonirinaanthony@gmail.com>
2025-02-19 23:30:02 +0300

    Fix allowedHosts for Railway

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   server: {
     port: 3000,  // Définit le port sur 3000 pour Railway
     host: "0.0.0.0",
-    strictPort: true   // Empêche Vite de changer le port
+    strictPort: true,  // Empêche Vite de changer le port
+    allowedHosts: ["ia-anthony-ranto.up.railway.app"] // Autorise Railway à accéder au serveur
   },
   preview: {
     port: 3000,  // Port pour `vite preview`
-    host: "0.0.0.0",
-    allowedHosts: ["IA-Anthony-Ranto.up.railway.app"]
+    host: "0.0.0.0"
   }
 });
